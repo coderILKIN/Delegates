ae9fd9b71fcdb7ea011adfe448bf9bcc93b74785
Ilkin Yusubov <tu758403c@code.edu.az>
2022-04-04 12:40:36 +0400

    delegate add

---
+++ Delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] PruductPrices = { 1.9, 25.7, 9.99, 4.8, 101.99, 12.6 };
+            Action<double[]> Info = TotalPriceCount;
+            Info(PruductPrices);
+
+        }
+        public static void TotalPriceCount(double[] Arr)
+        {
+            int count = Arr.Length;
+            double TotalPrice = default(int);
+            foreach (double item in Arr)
+            {
+                TotalPrice += item;
+            }
+            Console.WriteLine($"Total price of pruduct: {TotalPrice}$ , Pruducts count: {count}");
+        }
+    }
+}
